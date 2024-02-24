7fe504e60ab1ded7161b0c69f8c479866f789b91
Sean Harrigan <SeanHarrigan8@gmail.com>
2024-02-24 17:13:41 -0600

    html routes completed, server js and dependencies installed

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+}
+);
+
